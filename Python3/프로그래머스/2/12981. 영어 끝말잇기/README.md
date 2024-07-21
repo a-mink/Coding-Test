6a95b9302b16a6a7f4d41d9d5a585d0cbd647b78
a-mink <129038385+a-mink@users.noreply.github.com>
2024-07-21 19:30:17 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ Python3/프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.09 ms
+메모리: 10.2 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 21일 13:09:50
+2024년 07월 21일 19:30:16
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ Python3/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,7 +1,23 @@
-
 def solution(n, words):
-    for i in range(1, len(words)):
-        # 현재 단어의 첫 글자가 이전 단어의 마지막 글자와 다르거나, 현재 단어가 이전에 이미 나온 경우
-        if words[i][0] != words[i-1][-1] or words[i] in words[:i]:
-            return [(i % n) + 1, (i // n) + 1]
-    return [0, 0]
+    answer = []
+    # 마지막 단어(끝말잇기 이어지지 않는 단어) 제외.
+    for i, j in enumerate(words[:-1]):
+        # i + 1 인덱스 값의 정답여부 판별
+        if j in words[:i]:
+            answer.append(i%n + 1)
+            answer.append(i//n + 1)
+            break
+        elif j[-1] != words[i+1][0]:
+            answer.append((i + 1)%n + 1)
+            answer.append((i + 1)//n + 1)
+            break
+    
+    # 마지막 단어 점검 및 모두 정답일 케이스
+    if answer != []:
+        return answer
+    elif words[-1] in words[:-1]:
+        answer.append((len(words) - 1)%n + 1)
+        answer.append((len(words) - 1)//n + 1)
+        return answer
+    else:
+        return [0, 0]
